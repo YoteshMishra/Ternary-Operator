40c675e636da9de342d25fa3a3ce87e79826304b
YoteshMishra <yoteshmishra123@gmail.com>
2024-03-20 21:49:07 +0530

    ternaryOperator

---
+++ ternary.js
@@ -0,0 +1,20 @@
+// to replace if/else
+const a = 10;
+const b = 7;
+b > a ? 'True' : 'False'
+console.log(a, b);
+
+// if/else
+const y = 64;
+
+function Condition() {
+  y = 84;
+ return
+if(y%2 === 0){
+'true';
+}
+else{
+'false'
+};
+}
+console.log(Condition);
